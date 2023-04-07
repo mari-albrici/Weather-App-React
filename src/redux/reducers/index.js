c5b9d8a9ef8abff5bece1a978ec3abdbffccd1aa
marialbrici <marianna.albrici@gmail.com>
2023-04-07 10:57:32 +0200

    First commit

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,15 @@
+const initialState = {
+	location: {
+		latitude: '',
+		longitude: '',
+	},
+};
+
+const mainReducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+export default mainReducer;
